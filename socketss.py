137889b6d4cc9f3e7daa1fbcafe8957a3f7670a0
baddwolff <40377333+baddwolff@users.noreply.github.com>
2018-12-01 15:58:27 +0530

    ok

---
+++ socketss.py
@@ -0,0 +1,49 @@
+udp c
+from socket import *
+serverName = ‘hostname’
+serverPort = 12000
+clientSocket = socket(socket.AF_INET, socket.SOCK_DGRAM)
+message = raw_input(’Input lowercase sentence:’)
+clientSocket.sendto(message,(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print modifiedMessage
+clientSocket.close()
+
+
+
+from socket import *
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind((’’, serverPort))
+print ”The server is ready to receive”
+while 1:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage = message.upper()
+    serverSocket.sendto(modifiedMessage, clientAddress)
+
+
+tcp c
+from socket import *
+serverName = ’servername’
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence = raw_input(‘Input lowercase sentence:’)
+clientSocket.send(sentence)
+modifiedSentence = clientSocket.recv(1024)
+print ‘From Server:’, modifiedSentence
+clientSocket.close()
+
+
+from socket import *
+serverPort = 12000
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind((‘’,serverPort))
+serverSocket.listen(1)
+print ‘The server is ready to receive’
+while 1:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024)
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence)
+    connectionSocket.close()
